4b62781736d9a047d3639d67f1a018d99df59191
zzzQuyenProzzz <phan.duc.quyen@sun-asterisk.com>
2025-08-06 17:11:23 +0700

    [Task][90424] Create MVC Project

---
+++ WeatherApp/WeatherApp/Core/AppDelegate.swift
@@ -0,0 +1,72 @@
+//
+//  AppDelegate.swift
+//  WeatherApp
+//
+//  Created by Phan Quyen on 06/08/2025.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print("App did finish launching")
+        setupInitialConfigurations()
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        saveContext()
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        saveContext()
+    }
+    
+    // MARK: - Initial Setup
+    
+    private func setupInitialConfigurations() {
+        // Setup any initial app configurations here
+        print("Setting up initial configurations...")
+    }
+
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "WeatherApp")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+}
+	
